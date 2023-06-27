bc11e3ddda02c360eb2c36c7aaf3a412a440921e
edinilson sousa <dini.badboy@hotmail.com>
2023-06-27 02:40:47 -0300

    atualizacoes dos groups, tactics e details

--- lib/src/feature/home/view/widget/account.dart
+++ lib/src/feature/home/view/widget/account.dart
@@ -2,19 +2,52 @@ import 'package:flutter/material.dart';
 import 'package:localization/localization.dart';
 import 'package:mitre_app/src/common/app_bar.dart';
 
-class UserAccount extends StatelessWidget {
-  const UserAccount({super.key});
+class UserAccount extends StatefulWidget {
+  const UserAccount({Key? key}) : super(key: key);
+
+  @override
+  _UserAccountState createState() => _UserAccountState();
+}
+
+class _UserAccountState extends State<UserAccount> {
+  bool _isDarkMode = false;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: appBarCustom('app_name'.i18n(), subtitle: 'groups'.i18n()),
-      body: SingleChildScrollView(
-        child: Center(
-            child: Text('account'.i18n(),
-                style: const TextStyle(fontSize: 48, fontFamily: 'Billabong'))),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: _isDarkMode ? ThemeData.dark() : ThemeData.light(),
+      home: Scaffold(
+        appBar: appBarCustom('app_name'.i18n(), subtitle: 'groups'.i18n()),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Text(
+              'account'.i18n(),
+              style: const TextStyle(fontSize: 48, fontFamily: 'Billabong'),
+            ),
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: _isDarkMode ? Colors.black : Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(
+                  icon: Icon(_isDarkMode ? Icons.brightness_7 : Icons.brightness_4),
+                  color: _isDarkMode ? Colors.white : Colors.black,
+                  onPressed: () {
+                    setState(() {
+                      _isDarkMode = !_isDarkMode;
+                    });
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
 }
-  
--- lib/src/feature/home/view/widget/groups.dart
+++ lib/src/feature/home/view/widget/groups.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+/* import 'package:flutter/material.dart';
 import 'package:localization/localization.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:mitre_app/assets/mock/mitre_data/all_groups.dart';
@@ -6,7 +6,7 @@ import 'package:mitre_app/src/common/app_bar.dart';
 import 'package:mitre_app/src/feature/home/view/widget/groups_details.dart';
 
 class Groups extends StatefulWidget {
-  const Groups({super.key});
+  const Groups({Key? key});
 
   @override
   State<Groups> createState() => _GroupsState();
@@ -18,94 +18,78 @@ class _GroupsState extends State<Groups> {
     final CarouselController _controller = CarouselController();
 
     return Expanded(
-      child: Column(
-        children: [
-          CarouselSlider(
-            options: CarouselOptions(
-                autoPlay: false,
-                onPageChanged: (index, reason) {
-                  setState(() {
-                    print(index);
-                    _current = index;
-                  });
-                }),
-            carouselController: _controller,
-            items: MITRE_GROUPS
-                .map((dynamic item) => Center(
-                      child: Card(
-                        elevation: 5,
-                        shape: RoundedRectangleBorder(
-                          side: BorderSide(
-                            color: Theme.of(context).colorScheme.outline,
-                          ),
-                          borderRadius:
-                              const BorderRadius.all(Radius.circular(12)),
-                        ),
-                        child: InkWell(
-                          splashColor:
-                              Color.fromARGB(255, 1, 48, 87).withAlpha(30),
-                          onTap: () {
-                            debugPrint('Card tapped.');
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) =>
-                                    GroupsDetails(item['group_alias']),
-                              ),
-                            );
-                          },
-                          child: SizedBox(
-                            width: MediaQuery.of(context).size.width * 0.8,
-                            height: MediaQuery.of(context).size.height * 0.22,
-                            child: Padding(
-                              padding: const EdgeInsets.all(18.0),
-                              child: Column(
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  Text(item['group_alias'].toString(),
-                                      style: const TextStyle(fontSize: 16)),
-                                  const SizedBox(
-                                    height: 10,
-                                  ),
-                                  Text(item['group_aliases'].toString(),
-                                      style: const TextStyle(fontSize: 14)),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ))
-                .toList(),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: MITRE_GROUPS.asMap().entries.map((entry) {
-              return GestureDetector(
-                onTap: () {
-                  setState(() {
-                    print('guia' + entry.key.toString());
-                    // _current = entry.key;
-                    // _controller.jumpToPage(entry.key);
-                  });
-                },
-                child: Container(
-                  width: 12.0,
-                  height: 12.0,
-                  margin: const EdgeInsets.symmetric(
-                      vertical: 8.0, horizontal: 4.0),
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: (Theme.of(context).brightness == Brightness.dark
-                              ? Colors.white
-                              : Colors.black)
-                          .withOpacity(_current == entry.key ? 0.9 : 0.4)),
-                  // .withOpacity(0.4)),
+      child: CarouselSlider(
+        options: CarouselOptions(
+          autoPlay: false,
+          onPageChanged: (index, reason) {
+            setState(() {
+              print(index);
+              _current = index;
+            });
+          },
+          aspectRatio: 1, // Define uma proporção de 1 para criar um formato quadrado
+          viewportFraction: 0.45, // Controla a quantidade de espaço ocupado pelos Cards na tela
+          enlargeCenterPage: true, // Amplia o Card central
+          enlargeStrategy: CenterPageEnlargeStrategy.height,
+          // enlargeStrategy: CenterPageEnlargeStrategy.scale,
+        ),
+        carouselController: _controller,
+        items: MITRE_GROUPS.map((dynamic item) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: InkWell(
+              onTap: () {
+                debugPrint('Card tapped.');
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => GroupsDetails(item['group_alias']),
+                  ),
+                );
+              },
+              child: Card(
+                elevation: 5,
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(
+                    color: Theme.of(context).colorScheme.outline,
+                  ),
+                  borderRadius: BorderRadius.circular(6),
                 ),
-              );
-            }).toList(),
-          ),
-        ],
+                child: Container(
+                  width: double.infinity,
+                  height: double.infinity, // Define a mesma altura e largura para criar um formato quadrado
+                  padding: const EdgeInsets.all(10.0),
+                  margin: const EdgeInsets.all(8.0), // Diminui a distância entre os quadrados
+                  decoration: BoxDecoration(
+                    color: Colors.black, // Define a cor de fundo como preto
+                    borderRadius: BorderRadius.circular(6),
+                  ),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: const Color(0xFFFFCC80), // Define a cor laranja claro
+                      borderRadius: BorderRadius.circular(6),
+                    ),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          item['group_alias'].toString(),
+                          style: const TextStyle(fontSize: 14),
+                        ),
+                        const SizedBox(height: 5),
+                        Text(
+                          item['group_aliases'].toString(),
+                          style: const TextStyle(fontSize: 12),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          );
+        }).toList(),
       ),
     );
   }
@@ -113,14 +97,16 @@ class _GroupsState extends State<Groups> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: appBarCustom('app_name'.i18n(), subtitle: 'groups'.i18n()),
+      appBar: appBarCustom('app_name'.i18n()),
       body: Padding(
         padding: const EdgeInsets.all(18.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Text('what_are_groups_title'.i18n(),
-                style: const TextStyle(fontSize: 18, fontFamily: 'Billabong')),
+            Text(
+              'what_are_groups_title'.i18n(),
+              style: const TextStyle(fontSize: 18, fontFamily: 'Billabong'),
+            ),
             const SizedBox(
               height: 20,
             ),
@@ -129,6 +115,120 @@ class _GroupsState extends State<Groups> {
               style: const TextStyle(fontSize: 14),
             ),
             carouselWithIndicatorDemo(context),
+          ],
+        ),
+      ),
+    );
+  }
+}
+ */
+
+
+import 'package:flutter/material.dart';
+import 'package:localization/localization.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:mitre_app/assets/mock/mitre_data/all_groups.dart';
+import 'package:mitre_app/src/common/app_bar.dart';
+import 'package:mitre_app/src/feature/home/view/widget/groups_details.dart';
+
+class Groups extends StatelessWidget {
+  const Groups({Key? key});
+
+  Widget buildCarousel(BuildContext context) {
+    final CarouselController _controller = CarouselController();
+
+    return Expanded(
+      child: CarouselSlider.builder(
+        itemCount: MITRE_GROUPS.length,
+        options: CarouselOptions(
+          autoPlay: false,
+          aspectRatio: 1,
+          viewportFraction: 0.45,
+          enlargeCenterPage: true,
+          enlargeStrategy: CenterPageEnlargeStrategy.height,
+        ),
+        carouselController: _controller,
+        itemBuilder: (BuildContext context, int index, int realIndex) {
+          final item = MITRE_GROUPS[index];
+
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: InkWell(
+              onTap: () {
+                debugPrint('Card tapped.');
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => GroupsDetails(item['group_alias']),
+                  ),
+                );
+              },
+              child: Card(
+                elevation: 5,
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(
+                    color: Theme.of(context).colorScheme.outline,
+                  ),
+                  borderRadius: BorderRadius.circular(6),
+                ),
+                child: Container(
+                  width: double.infinity,
+                  height: double.infinity,
+                  padding: const EdgeInsets.all(10.0),
+                  margin: const EdgeInsets.all(8.0),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    borderRadius: BorderRadius.circular(6),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        item['group_alias'].toString(),
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: (item['group_alias'] == 'apt12' || item['group_alias'] == 'apt30') ? Colors.black : Colors.orange,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      Text(
+                        item['group_aliases'].toString(),
+                        style: const TextStyle(fontSize: 12),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBarCustom('app_name'.i18n()),
+      body: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text(
+              'what_are_groups_title'.i18n(),
+              style: TextStyle(
+                fontSize: 28,
+                fontFamily: 'Billabong',
+                color: Colors.orange,
+              ),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'what_are_groups_desc'.i18n(),
+              style: const TextStyle(fontSize: 14),
+            ),
+            buildCarousel(context),
           ],
         ),
       ),
