1b3f9bd53ac575c015c5757425c424b08f3af431
patrickmarcoss <patrick.marcos.2013@gmail.com>
2023-06-27 23:25:47 -0300

    Correção de erros na login page, cadastro e esqueci a senha

--- lib/src/feature/auth/view/page/login_page.dart
+++ lib/src/feature/auth/view/page/login_page.dart
@@ -18,15 +18,6 @@ class _LoginPageState extends State<LoginPage> {
   final _formKey = GlobalKey<FormState>();
   final _viewModel = Modular.get<LoginViewModel>();
 
-  List<Color> backgroundColors = [
-    Color.fromARGB(255, 101, 182, 248),
-    Color.fromARGB(255, 154, 248, 157),
-    Color.fromARGB(255, 247, 243, 243),
-    Colors.yellow,
-    Color.fromARGB(255, 233, 176, 243)
-  ];
-  int currentBackgroundColorIndex = 0;
-
   Widget _buildHiddenButton() {
     return IconButton(
       onPressed: () {
@@ -37,131 +28,112 @@ class _LoginPageState extends State<LoginPage> {
       icon: Icon(obscureText ? Icons.visibility_off : Icons.visibility_sharp),
     );
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: AnimatedContainer(
-        duration: Duration(seconds: 1),
-        color: backgroundColors[currentBackgroundColorIndex],
-        child: Center(
-          child: SingleChildScrollView(
-            padding: const EdgeInsets.all(16),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Center(
-                    child: Text(
-                      'sign_in'.i18n(),
-                      style: const TextStyle(
-                        fontSize: 24.0,
-                        color: Color.fromARGB(255, 3, 3, 3),
-                        fontWeight: FontWeight.bold,
-                      ),
+      body: Center(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Center(
+                  child: Text(
+                    'sign_in'.i18n(),
+                    style: const TextStyle(
+                      fontSize: 24.0,
+                      color: Color.fromARGB(255, 3, 3, 3),
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                  CustomFormTextField(
-                    controller: _emailController,
-                    labelText: 'email'.i18n(),
-                    hintText: 'example@email.com'.i18n(),
-                    icon: const Icon(Icons.email),
-                    validatorFunction: (email) =>
-                        _viewModel.emailValidator(email),
+                ),
+                CustomFormTextField(
+                  controller: _emailController,
+                  labelText: 'email'.i18n(),
+                  hintText: 'example@email.com'.i18n(),
+                  icon: const Icon(Icons.email),
+                  validatorFunction: (email) =>
+                      _viewModel.emailValidator(email),
+                ),
+                CustomFormTextField(
+                  controller: _passwordController,
+                  obscureText: obscureText,
+                  labelText: 'password'.i18n(),
+                  hintText: 'password'.i18n(),
+                  icon: _buildHiddenButton(),
+                  validatorFunction: (password) =>
+                      _viewModel.passwordValidator(password),
+                ),
+                const SizedBox(height: 12),
+                const Divider(),
+                ElevatedButton(
+                  onPressed: () async {
+                    await _viewModel.login(
+                      _formKey,
+                      _emailController,
+                      _passwordController,
+                    );
+                    if (_viewModel.isLogged) {
+                      Navigator.pushNamed(context, '/home');
+                    }
+                  },
+                  child: Text('Login'.toUpperCase()),
+                  style: ElevatedButton.styleFrom(
+                    primary: Color.fromARGB(202, 238, 88, 2),
+                    onPrimary: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    padding: const EdgeInsets.symmetric(vertical: 16.0),
                   ),
-                  CustomFormTextField(
-                    controller: _passwordController,
-                    obscureText: obscureText,
-                    labelText: 'password'.i18n(),
-                    hintText: 'password'.i18n(),
-                    icon: _buildHiddenButton(),
-                    validatorFunction: (password) =>
-                        _viewModel.passwordValidator(password),
-                  ),
-                  const SizedBox(height: 12),
-                  const Divider(),
-                  ElevatedButton(
-                    onPressed: () async {
-                      await _viewModel.login(
-                        _formKey,
-                        _emailController,
-                        _passwordController,
-                      );
-                      if (_viewModel.isLogged) {
-                        Navigator.pushNamed(context, '/home');
-                      }
-                    },
-                    child: Text('Login'.toUpperCase()),
-                    style: ElevatedButton.styleFrom(
-                      primary: Color.fromARGB(202, 238, 88, 2),
-                      onPrimary: Colors.white,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8.0),
-                      ),
-                      padding: const EdgeInsets.symmetric(vertical: 16.0),
+                ),
+                const SizedBox(height: 36),
+                TextButton(
+                  onPressed: () async {
+                    Navigator.pushNamed(context, 'sign-up');
+                  },
+                  child: Text(
+                    'Cadastrar',
+                    style: TextStyle(
+                      color: Color.fromARGB(202, 238, 88, 2),
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                  const SizedBox(height: 36),
-                  TextButton(
-                    onPressed: () async {
-                      Navigator.pushNamed(context, 'sign-up');
-                    },
-                    child: Text(
-                      'Cadastrar',
-                      style: TextStyle(
-                        color: Color.fromARGB(202, 238, 88, 2),
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.bold,
-                      ),
+                ),
+                const SizedBox(height: 12),
+                TextButton(
+                  onPressed: () async {
+                    Navigator.pushNamed(context, 'forgot-password');
+                  },
+                  child: Text(
+                    'Esqueceu a senha?',
+                    style: TextStyle(
+                      color: Color.fromARGB(202, 238, 88, 2),
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                  const SizedBox(height: 12),
-                  TextButton(
-                    onPressed: () async {
-                      Navigator.pushNamed(context, 'forgot-password');
-                    },
-                    child: Text(
-                      'Esqueceu a senha?',
-                      style: TextStyle(
-                        color: Color.fromARGB(202, 238, 88, 2),
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.bold,
-                      ),
+                ),
+                const SizedBox(height: 30.0),
+                Center(
+                  child: Text(
+                    'app_name'.i18n(),
+                    style: const TextStyle(
+                      color: Color.fromARGB(255, 2, 2, 2),
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                  const SizedBox(height: 30.0),
-                  Center(
-                    child: Text(
-                      'app_name'.i18n(),
-                      style: const TextStyle(
-                        color: Color.fromARGB(255, 2, 2, 2),
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
       ),
     );
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    startBackgroundColorAnimation();
-  }
-
-  void startBackgroundColorAnimation() {
-    Future.delayed(Duration(seconds: 3), () {
-      setState(() {
-        currentBackgroundColorIndex =
-            (currentBackgroundColorIndex + 1) % backgroundColors.length;
-        startBackgroundColorAnimation();
-      });
-    });
   }
 }
--- lib/src/feature/auth/view/page/register.dart
+++ lib/src/feature/auth/view/page/register.dart
@@ -22,15 +22,6 @@ class _RegisterPageState extends State<RegisterPage> {
   final _formKey = GlobalKey<FormState>();
   final _viewModel = Modular.get<RegisterViewModel>();
 
-  List<Color> backgroundColors = [
-    Color.fromARGB(255, 144, 199, 243),
-    Color.fromARGB(255, 131, 245, 135),
-    Color.fromARGB(255, 252, 252, 252),
-    Colors.yellow,
-    Color.fromARGB(255, 238, 154, 253)
-  ];
-  int currentBackgroundColorIndex = 0;
-
   Widget hideShowIconButton() {
     return IconButton(
       onPressed: () {
@@ -67,95 +58,90 @@ class _RegisterPageState extends State<RegisterPage> {
     bool successRegister;
 
     return Scaffold(
-      body: AnimatedContainer(
-        duration: Duration(seconds: 1),
-        color: backgroundColors[currentBackgroundColorIndex],
-        child: Center(
-          child: SingleChildScrollView(
-            padding: const EdgeInsets.all(16),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  CustomFormTextField(
-                    controller: _nameController,
-                    labelText: 'name'.i18n(),
-                    hintText: 'your_name'.i18n(),
-                    validatorFunction: (name) => _viewModel.nameValidator(name),
-                  ),
-                  CustomFormTextField(
-                    controller: _usernameController,
-                    labelText: 'username'.i18n(),
-                    hintText: 'your_username'.i18n(),
-                    validatorFunction: (fullName) =>
-                        _viewModel.nameValidator(fullName),
-                  ),
-                  CustomFormTextField(
-                    controller: _emailController,
-                    labelText: 'email'.i18n(),
-                    hintText: 'example@email.com'.i18n(),
-                    icon: const Icon(Icons.email_outlined, color: Color.fromARGB(255, 0, 0, 0)),
-                    validatorFunction: (email) =>
-                        _viewModel.emailValidator(email),
-                  ),
-                  CustomFormTextField(
-                    controller: _passwordController,
-                    obscureText: obscureText,
-                    labelText: 'password'.i18n(),
-                    hintText: 'password'.i18n(),
-                    icon: hideShowIconButton(),
-                    validatorFunction: (password) =>
-                        _viewModel.passwordValidator(password),
-                  ),
-                  CustomFormTextField(
-                    controller: _passwordConfirmController,
-                    obscureText: obscureText,
-                    labelText: 'confirm_password'.i18n(),
-                    hintText: 'confirm_password'.i18n(),
-                    icon: hideShowIconButton(),
-                    validatorFunction: (confirmPassword) =>
-                        _viewModel.passwordConfirmValidator(
-                            confirmPassword, _passwordController.text),
-                  ),
-                  const SizedBox(height: 12),
-                  TextButton(
-                    onPressed: () async =>
-                        Navigator.pushNamed(context, '/auth'),
-                    child: Text(
-                      'back_login_page'.i18n(),
-                      style: TextStyle(
-                        color: Color.fromARGB(202, 238, 88, 2),
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.bold,
-                      ),
+      body: Center(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                CustomFormTextField(
+                  controller: _nameController,
+                  labelText: 'name'.i18n(),
+                  hintText: 'your_name'.i18n(),
+                  validatorFunction: (name) => _viewModel.nameValidator(name),
+                ),
+                CustomFormTextField(
+                  controller: _usernameController,
+                  labelText: 'username'.i18n(),
+                  hintText: 'your_username'.i18n(),
+                  validatorFunction: (fullName) =>
+                      _viewModel.nameValidator(fullName),
+                ),
+                CustomFormTextField(
+                  controller: _emailController,
+                  labelText: 'email'.i18n(),
+                  hintText: 'example@email.com'.i18n(),
+                  icon: const Icon(Icons.email_outlined, color: Color.fromARGB(255, 0, 0, 0)),
+                  validatorFunction: (email) =>
+                      _viewModel.emailValidator(email),
+                ),
+                CustomFormTextField(
+                  controller: _passwordController,
+                  obscureText: obscureText,
+                  labelText: 'password'.i18n(),
+                  hintText: 'password'.i18n(),
+                  icon: hideShowIconButton(),
+                  validatorFunction: (password) =>
+                      _viewModel.passwordValidator(password),
+                ),
+                CustomFormTextField(
+                  controller: _passwordConfirmController,
+                  obscureText: obscureText,
+                  labelText: 'confirm_password'.i18n(),
+                  hintText: 'confirm_password'.i18n(),
+                  icon: hideShowIconButton(),
+                  validatorFunction: (confirmPassword) =>
+                      _viewModel.passwordConfirmValidator(
+                          confirmPassword, _passwordController.text),
+                ),
+                const SizedBox(height: 12),
+                TextButton(
+                  onPressed: () async =>
+                      Navigator.pushNamed(context, '/auth'),
+                  child: Text(
+                    'back_login_page'.i18n(),
+                    style: TextStyle(
+                      color: Color.fromARGB(202, 238, 88, 2),
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                  const SizedBox(height: 36),
-                  CustomElevatedButton(
-                    text: 'sign_up'.i18n(),
-                    onPressed: () async {
-                      successRegister = await _viewModel.register(
-                        _formKey,
-                        _nameController,
-                        _emailController,
-                        _usernameController,
-                        _passwordController,
-                      );
-                      if (successRegister) {
-                        showAlertDialog(context, successRegister.toString());
-                      } else {
-                        showAlertDialog(context, successRegister.toString());
-                      }
-                    },
-                  ),
-                ],
-              ),
+                ),
+                const SizedBox(height: 36),
+                CustomElevatedButton(
+                  text: 'sign_up'.i18n(),
+                  onPressed: () async {
+                    successRegister = await _viewModel.register(
+                      _formKey,
+                      _nameController,
+                      _emailController,
+                      _usernameController,
+                      _passwordController,
+                    );
+                    if (successRegister) {
+                      showAlertDialog(context, successRegister.toString());
+                    } else {
+                      showAlertDialog(context, successRegister.toString());
+                    }
+                  },
+                ),
+              ],
             ),
           ),
         ),
       ),
     );
   }
-
 }
